0bad93acc141349dd65349be58255aa445115db2
Christopher Lewis <cjoellewis@yahoo.com>
2020-04-26 19:04:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array)
+  array = [25, 7, 1]
+  array.sort
+end
 
+def sort_array_desc(integer)
+  integer = [25, 7, 14]
+  integer.sort.reverse
+end
+
+def sort_array_char_count(characters)
+  characters = ["dogs", "cat", "Horses"]
+  characters.sort_by(&:length)
+end
+
+def swap_elements(names)
+  names = ["blake", "ashley", "scott"]
+  names[1], names[2] = names[2], names[1]
+  names
+end
+
+def reverse_array(integers)
+  integers = [12, 4, 35]
+  integers.reverse
+end
+
+def kesha_maker(dollar)
+  dollar = ["blake", "ashley", "scott"]
+  
